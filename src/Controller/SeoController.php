055b1436de1a7d7d0c3e839c81458e4dba9b7b9c
nejuras <nerusha@gmail.com.com>
2018-04-12 14:41:10 +0300

    seo-symfony-homework

---
+++ src/Controller/SeoController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class SeoController extends Controller
+{
+    /**
+     * @Route("/studentai", name="studentai")
+     */
+    public function index()
+    {
+        $url = "/var/www/html/wish-a-gift/public/json/data.json";
+
+        $json = file_get_contents($url);
+        $data = json_decode($json, true);
+
+        return $this->render('seo/index.html.twig', [
+            'title' => 'Studentai',
+            'data' => $data
+        ]);
+    }
+
+    /**
+     * @Route("/akademija", name="akademija")
+     */
+    public function indefx(Request $request)
+    {
+
+        $utm_campaign = $request->get('utm_campaign');
+        $utm_term = $request->get('utm_term');
+        $utm_content = $request->get('utm_content');
+
+        $url = "/var/www/html/wish-a-gift/public/json/data.json";
+
+        $json = file_get_contents($url);
+        $data = json_decode($json, true);
+
+        return $this->render('seo/vertinimas.html.twig', [
+            'title' => 'Akademija',
+            'utm_campaign' => $utm_campaign,
+            'utm_term' => $utm_term,
+            'utm_content' => $utm_content,
+            'data' => $data
+        ]);
+    }
+
+
+}
