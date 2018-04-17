812f0539562dbfbf89a11f85cd3b43c6b2945775
nejuras <nerusha@gmail.com>
2018-04-17 11:07:53 +0300

    changed public to private

--- src/Controller/SeoController.php
+++ src/Controller/SeoController.php
@@ -2,28 +2,27 @@
 
 namespace App\Controller;
 
-//use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class SeoController extends Controller
 {
-    public $urlPart = "/public/json/data.json";
+    private $urlPart = "/public/json/data.json";
     /**
      * @Route("/studentai", name="studentai")
      */
 
     public function index()
     {
-        //var_dump($this->urlPart);
         $url = $this->get('kernel')->getProjectDir() . $this->urlPart;
         $json = file_get_contents($url);
         $data = json_decode($json, true);
-        return $this->render('seo/index.html.twig', [
+        return $this->render('seo/index.html.twig',
+            [
             'title' => 'Studentai',
             'data' => $data
-        ]);
+            ]);
     }
 
     /**
@@ -39,12 +38,13 @@ class SeoController extends Controller
         $json = file_get_contents($url);
         $data = json_decode($json, true);
 
-        return $this->render('seo/vertinimas.html.twig', [
+        return $this->render('seo/vertinimas.html.twig',
+            [
             'title' => 'Akademija',
             'utm_campaign' => $utm_campaign,
             'utm_term' => $utm_term,
             'utm_content' => $utm_content,
             'data' => $data
-        ]);
+            ]);
     }
 }
