3abf495a4d74984181862a9d194361df26eaa37d
nejuras <nerusha@gmail.com>
2018-04-17 11:30:12 +0300

    update

--- src/Controller/SeoController.php
+++ src/Controller/SeoController.php
@@ -9,15 +9,16 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class SeoController extends Controller
 {
     private $urlPart = "/public/json/data.json";
+
     /**
      * @Route("/studentai", name="studentai")
      */
-
     public function index()
     {
         $url = $this->get('kernel')->getProjectDir() . $this->urlPart;
         $json = file_get_contents($url);
         $data = json_decode($json, true);
+        
         return $this->render('seo/index.html.twig',
             [
             'title' => 'Studentai',
@@ -30,7 +31,6 @@ class SeoController extends Controller
      */
     public function akademija(Request $request)
     {
-
         $utm_campaign = $request->get('utm_campaign');
         $utm_term = $request->get('utm_term');
         $utm_content = $request->get('utm_content');
